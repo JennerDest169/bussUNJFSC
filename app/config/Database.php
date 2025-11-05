c5be6a8b2411f33b1c730dfa85ca9390c1c3e8fc
12345renzo <renzogonzalesasencios@gmail.com>
2025-11-04 19:36:59 -0500

    db

---
+++ app/config/Database.php
@@ -0,0 +1,26 @@
+<?php
+class Database {
+    private $host = "172.17.0.3";
+    private $db_name = "sigtu_db";
+    private $username = "renzo";
+    private $password = "renzoGA16@";
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
